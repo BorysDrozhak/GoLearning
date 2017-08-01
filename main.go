8e8dd7a4a8e4582e5dcd55a70651c429f00b55b8
Borys Drozhak <borys.drozhak@gmail.com>
2017-08-01 12:52:22 +0300

    workable solution; hosts can be specifievia env variables as well as the command

--- main.go
+++ main.go
@@ -7,28 +7,27 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	user := "bdrozhak"
-	home := "/Users/" + user
-	private_key_path := "/.ssh/id_rsa"
-	ssh_password := "ypur password"
-	dstHost := "127.0.0.1:22"
-	command := "env"
+type config struct {
+	User       string
+	Signer     ssh.Signer
+	ClientConf *ssh.ClientConfig
+}
 
-	key, err := ioutil.ReadFile(home + private_key_path)
+func makeSigner(p string) ssh.Signer {
+	key, err := ioutil.ReadFile(p)
 	if err != nil {
 		log.Fatalf("unable to read private key: %v", key)
 	}
-
 	signer, err := ssh.NewSignerFromKey(key)
 	if err != nil {
 		log.Println("Private key is encrypted")
 		passPhrase := os.Getenv("PASSPHRASE")
 		if passPhrase == "" {
 			log.Println("PASSPHRASE is not set as env variable")
-			// TODO: ask passphrase in case
+			// TODO: ask paraphrase in case
 		}
 		rawKey, err := ssh.ParseRawPrivateKeyWithPassphrase(key, []byte(passPhrase))
 		signer, err = ssh.NewSignerFromKey(rawKey)
@@ -36,32 +35,87 @@ func main() {
 			log.Fatalf("unable to parse private key: %v", err)
 		}
 	}
-	config := &ssh.ClientConfig{
-		User: user,
+	return signer
+}
+
+func prepareSshConfig(s ssh.Signer, u string) *ssh.ClientConfig {
+
+	c := &ssh.ClientConfig{
+		User: u,
 		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-			ssh.Password(ssh_password),
+			ssh.PublicKeys(s),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-	client, err := ssh.Dial("tcp", dstHost, config)
-	if err != nil {
-		log.Fatal("Failed to dial: ", err)
+	return c
+}
+
+func makeUserConf() *config {
+	var c config
+
+	c.User = os.Getenv("USER")
+	if c.User == "" {
+		log.Fatal("User is not defined")
 	}
 
-	session, err := client.NewSession()
-	if err != nil {
-		log.Fatal("Failed to create session: ", err)
-	}
-	defer session.Close()
+	home := "/Users/" + c.User
+	private_key_path := "/.ssh/id_rsa"
+	p := home + private_key_path
 
-	// Once a Session is created, you can execute a single command on
-	// the remote side using the Run method.
-	var b bytes.Buffer
-	session.Stdout = &b
-	if err := session.Run(command); err != nil {
-		log.Fatal("Failed to run: " + err.Error())
-	}
+	signer := makeSigner(p)
+	c.ClientConf = prepareSshConfig(signer, c.User)
+	return &c
+}
 
-	fmt.Println(b.String())
+func execCommand(h string, command string, conf *config) (c chan string) {
+	c = make(chan string)
+	go func() {
+		fmt.Println("enter host:", h)
+		client, err := ssh.Dial("tcp", h, conf.ClientConf)
+		if err != nil {
+			log.Fatal("Failed to dial: ", err)
+		}
+		defer client.Close()
+
+		session, err := client.NewSession()
+		if err != nil {
+			log.Fatal("Failed to create session: ", err)
+		}
+		defer session.Close()
+
+		// Once a Session is created, you can execute a single command on
+		// the remote side using the Run method.
+		// TODO: up to 20 hosts; run a command simultaneity .. .. .
+		var b bytes.Buffer
+		session.Stdout = &b
+		if err := session.Run(command); err != nil {
+			log.Fatal("Failed to run: " + err.Error())
+		}
+
+		c <- b.String()
+	}()
+	return
+}
+
+func main() {
+	c := makeUserConf()
+
+	dstHosts := strings.Split(os.Getenv("H"), " ")
+	if len(dstHosts) == 0 {
+		log.Fatal("Destination Host is not defined")
+	}
+	command := os.Getenv("C")
+	if command == "" {
+		command = "env"
+	}
+	command = "date; hostname; sleep 3 ; date"
+	//dstHosts="127.0.0.1:22 m104.sjc.opendns.com:22 graphite013.sjc.opendns.com:22 graphite011.sjc.opendns.com:22"
+	var workers []chan string
+
+	for _, dstHost := range dstHosts {
+		workers = append(workers, execCommand(dstHost, command, c))
+	}
+	for _, w := range workers {
+		fmt.Println(<- w)
+	}
 }
