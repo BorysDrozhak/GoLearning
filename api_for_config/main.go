8e56aaea0ae91db17d7aab090bca26f2146511b8
Borys Drozhak <borys.drozhak@gmail.com>
2017-09-09 00:51:49 +0300

    better code but still can't serialize properly

--- api_for_config/main.go
+++ api_for_config/main.go
@@ -10,17 +10,19 @@ import (
 	"fmt"
 )
 
-const config_file_name = "config.yml"
+const configFileName = "config.yml"
+const serverListenAddress = "127.0.0.1:8090"
 
 type StructConfig struct {
-	A string `yaml:"a"`
-	B string `yaml:"b"`
+	aggregation string `yaml:"aggregation"`
+	b int `yaml:"chatTimeout"`
 }
+
 
 func main() {
 	var configStruct StructConfig
 	pwd := "/Users/bdrozhak/IdeaProjects/GoLearning/src/github.com/BorysDrozhak/GoLearning/api_for_config/"
-	config_file_name_full_path := filepath.Join(pwd, config_file_name)
+	configFileNameFullPath := filepath.Join(pwd, configFileName)
 
 	r := gin.Default()
 	v0 := r.Group("/v0")
@@ -28,26 +30,51 @@ func main() {
 	v0.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"version": "v0",
 		})
 	})
-	v0.GET("/read", func(c *gin.Context) {
-		dat, err := ioutil.ReadFile(config_file_name_full_path)
+	v0.GET("/config/:rule", func(c *gin.Context) {
+		bytes, err := ioutil.ReadFile(configFileNameFullPath)
 		if err != nil {
 			log.Println("Can't read file: ", err)
 			c.Status(http.StatusInternalServerError)
-		} else {
-			err := yaml.Unmarshal(dat, &configStruct)
-			if err != nil {
-				log.Println("Can't read file: ", err)
-				c.Status(http.StatusInternalServerError)
-			} else {
-				c.JSON(200, gin.H{
-					"a": fmt.Sprintf("%v", configStruct.A),
-					"b": fmt.Sprintf("%v", configStruct.B),
-				})
-			}
+			return
 		}
-	})
+		//log.Println(string(bytes))
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+		err = yaml.Unmarshal(bytes, &configStruct)
+		if err != nil {
+			log.Println("Can't read file: ", err)
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		//fmt.Println(configStruct)
+		switch rule := c.Params.ByName("rule"); rule {
+			case "aggregation":
+				v, err := yaml.Marshal(configStruct.aggregation)
+				if err != nil {
+					log.Println("Can't serialize: ", err)
+					c.Status(http.StatusInternalServerError)
+					return
+				}
+				c.YAML(200, v)
+			case "chatTimeout":
+				b := fmt.Sprintf("%v", configStruct.b)
+				//fmt.Println(b)
+				c.JSON(200, b)
+			case "all":
+				c.JSON(200, fmt.Sprintf("%v", &configStruct))
+			default:
+				//data, err  := json.Marshal(&configStruct)
+				//if err != nil {
+				//	log.Println("Can't load to json: ", err)
+				//	c.Status(http.StatusInternalServerError)
+				//	return
+				log.Println("there is no such a rule: ", rule )
+				c.Status(http.StatusNotFound)
+				return
+			}
+		})
+	r.Run(serverListenAddress) // listen and serve
 }
